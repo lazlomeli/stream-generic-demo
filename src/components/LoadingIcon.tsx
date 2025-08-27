9ec6df5e73a3dd9e0659e0908f0a84a1f1bae865
Lazlo Meli <lazlomeli@gmail.com>
2025-08-27 12:34:57 +0200

    feat: new Stream loading icon

--- src/components/LoadingIcon.tsx
+++ src/components/LoadingIcon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoadingCircles from '../icons/loading-circles.svg';
+import StreamLoading from '../icons/stream-loading.svg';
 import './LoadingIcon.css';
 
 interface LoadingIconProps {
@@ -11,14 +11,14 @@ const LoadingIcon: React.FC<LoadingIconProps> = ({ size = 32, className = '' }) 
   return (
     <div className={`loading-icon ${className}`} style={{ width: size, height: size }}>
       <img 
-        src={LoadingCircles} 
+        src={StreamLoading} 
         alt="Loading..." 
         width="100%" 
         height="100%"
         style={{ 
           width: '100%',
           height: '100%',
-          filter: 'hue-rotate(240deg) saturate(1.2) brightness(1.1)'
+          filter: 'grayscale(1) brightness(0.6) opacity(0.2)'
         }}
       />
     </div>
